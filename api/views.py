f0574e7fd39a21bb62e4f1ea99d720c00fbf9b49
radulence84 <radulence84@gmail.com>
2025-03-21 14:57:26 +0100

    vezba

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Proizvod,Prodavnica
+from rest_framework import viewsets
+from .serializers import ProdavnicaSerializer, ProizvodSerializer
 
-# Create your views here.
+class ProdavnicaViewSet(viewsets.ModelViewSet):
+    queryset = Prodavnica.objects.all()
+    serializer_class = ProdavnicaSerializer
+
+class ProizvodViewSet(viewsets.ModelViewSet):
+    queryset = Proizvod.objects.all()
+    serializer_class = ProizvodSerializer
