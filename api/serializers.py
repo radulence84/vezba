5405c7c804ef8ad5820cdea5fbbe2a0b5cb796ac
radulence84 <radulence84@gmail.com>
2025-03-21 18:09:59 +0100

    vezba

--- api/serializers.py
+++ api/serializers.py
@@ -4,9 +4,9 @@ from rest_framework import serializers
 class ProdavnicaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prodavnica
-        fields = ['naziv_prodavnice','lokacija_prodavnice']
+        fields = ['id','naziv_prodavnice','lokacija_prodavnice']
 
 class ProizvodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Proizvod
-        fields = ['naziv_proizvoda','opis','cena','prodavnica']
+        fields = ['id','naziv_proizvoda','opis','cena','prodavnica']
