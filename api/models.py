1da807c6e9941cd529ebd7c71e96d6fcf938b449
radulence84 <radulence84@gmail.com>
2025-03-21 14:06:59 +0100

    vezba

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Prodavnica(models.Model):
+    naziv_prodavnice = models.CharField(max_length=50)
+    lokacija_prodavnice = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.naziv_prodavnice
+
+class Proizvod(models.Model):
+    naziv_prozvoda = models.CharField(max_length=50)
+    opis = models.TextField()
+    cena = models.DecimalField(max_digits=4 , decimal_places=2)
+    prodavnica = models.ForeignKey('Prodavnica', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.naziv_prozvoda
+
