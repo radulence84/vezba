58f5c77b0ddeb4e4b17ec0b34f5c7ba0cc0b7c84
radulence84 <radulence84@gmail.com>
2025-03-23 17:13:42 +0100

    vezba

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from .models import Proizvod,Prodavnica
 from rest_framework import viewsets
 from .serializers import ProdavnicaSerializer, ProizvodSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class ProdavnicaViewSet(viewsets.ModelViewSet):
     queryset = Prodavnica.objects.all()
     serializer_class = ProdavnicaSerializer
+    permission_classes = [IsAuthenticated]
 
 class ProizvodViewSet(viewsets.ModelViewSet):
     queryset = Proizvod.objects.all()
     serializer_class = ProizvodSerializer
+    permission_classes = [IsAuthenticated]
