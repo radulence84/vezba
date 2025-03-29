0d036868f751564fe911871d3337b37ac75e7540
Aleksandar Stojkovic <acast83@gmail.com>
2025-03-29 22:18:38 +0100

    fix

--- api/views.py
+++ api/views.py
@@ -4,7 +4,8 @@ from .models import Proizvod,Prodavnica
 from rest_framework import viewsets
 from .serializers import ProdavnicaSerializer, ProizvodSerializer
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.response import Response
+from rest_framework import status
 
 class ProdavnicaViewSet(viewsets.ModelViewSet):
     queryset = Prodavnica.objects.all()
@@ -12,8 +13,97 @@ class ProdavnicaViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
+    # CREATE action
+    def create(self, request, *args, **kwargs):
+        """Create"""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # LIST action§
+    def list(self, request, *args, **kwargs):
+        """List """
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # RETRIEVE action
+    def retrieve(self, request, *args, **kwargs):
+        """Retrieve """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        """Update instance"""
+        partial = kwargs.pop("partial", True)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        instance = self.perform_update(serializer)
+        response_data = self.get_serializer(instance).data
+        return Response(response_data)
+
+    def perform_update(self, serializer):
+        """Perform update instance"""
+        return  serializer.save()
+
 class ProizvodViewSet(viewsets.ModelViewSet):
     queryset = Proizvod.objects.all()
     serializer_class = ProizvodSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
+
+    # CREATE action
+    def create(self, request, *args, **kwargs):
+        """Create"""
+        payload = request.data
+        # payload["promo_cena"] = int(payload["cena"]) * 0.9
+        payload["created_by"] = request.user
+        serializer = self.get_serializer(data=payload)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception as e:
+            raise e
+        self.perform_create(serializer)
+        print("napravio sam novi proizvod")
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # LIST action§
+    def list(self, request, *args, **kwargs):
+        """List """
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # RETRIEVE action
+    def retrieve(self, request, *args, **kwargs):
+        """Retrieve """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        """Update instance"""
+        partial = kwargs.pop("partial", True)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        instance = self.perform_update(serializer)
+        response_data = self.get_serializer(instance).data
+        return Response(response_data)
+
+    def perform_update(self, serializer):
+        """Perform update instance"""
+        return serializer.save()
